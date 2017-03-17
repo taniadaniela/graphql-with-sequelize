ef0c18d2cc492dd59fb18c4ab1cc10599d79d388
tania <taniadaniela1803@gmail.com>
2017-03-16 21:18:45 -0300

    Added sequelize basic structure using postgres as dialect

--- db.js
+++ db.js
@@ -1,55 +1,56 @@
-import Sequelize from 'sequelize';
-import Faker from 'faker';
-import _ from 'lodash';
+// TODO point this to server structure
+// import Sequelize from 'sequelize';
+// import Faker from 'faker';
+// import _ from 'lodash';
 
-//db.js
-const Conn = new Sequelize(process.env.DATABASE_URL);
+// //db.js
+// const Conn = new Sequelize("postgres");
 
-const Person = Conn.define('person', {
-  firstName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  lastName: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    validate: {
-      isEmail: true
-    }
-  }
-});
+// const Person = Conn.define('person', {
+//   firstName: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   lastName: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   email: {
+//     type: Sequelize.STRING,
+//     validate: {
+//       isEmail: true
+//     }
+//   }
+// });
 
-const Post = Conn.define('post', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  content: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+// const Post = Conn.define('post', {
+//   title: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   },
+//   content: {
+//     type: Sequelize.STRING,
+//     allowNull: false
+//   }
+// });
 
-// Relations
-Person.hasMany(Post);
-Post.belongsTo(Person);
+// // Relations
+// Person.hasMany(Post);
+// Post.belongsTo(Person);
 
-Conn.sync({ force: true }).then(()=> {
-  _.times(10, ()=> {
-    return Person.create({
-      firstName: Faker.name.firstName(),
-      lastName: Faker.name.lastName(),
-      email: Faker.internet.email()
-    }).then(person => {
-      return person.createPost({
-        title: `Sample post by ${person.firstName}`,
-        content: 'here is some content'
-      });
-    });
-  });
-});
+// Conn.sync({ force: true }).then(()=> {
+//   _.times(10, ()=> {
+//     return Person.create({
+//       firstName: Faker.name.firstName(),
+//       lastName: Faker.name.lastName(),
+//       email: Faker.internet.email()
+//     }).then(person => {
+//       return person.createPost({
+//         title: `Sample post by ${person.firstName}`,
+//         content: 'here is some content'
+//       });
+//     });
+//   });
+// });
 
-export default Conn;
+// export default Conn;
